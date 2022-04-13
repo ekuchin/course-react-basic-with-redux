a94fbc4145c1a900509ba71858786f81e30de099
Eugene A. Kuchin <e.kuchin@gmail.com>
2022-04-13 09:19:23 +0500

    v1.0

--- src/components/ClassCat.js
+++ src/components/ClassCat.js
@@ -3,13 +3,23 @@ import React from "react";
 class ClassCat extends React.Component{
 
     render(){
+        
+        const cat = {name:"Рамзес", breed:"Сфинкс", weight:2, isAngry:true}       
+        const logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/GreekSphynxCat1.png/274px-GreekSphynxCat1.png"
+
+        const catStyle = {
+            color: 'white',
+            backgroundColor:'green'
+        }
+
         return(
             <div>
-                <h1>Кот:</h1>
-                <p>Имя: Рамзес</p>
-                <p>Порода: Сфинкс</p>
-                <p>Вес: 2кг.</p>
-                <p>Состояние: Сердит</p>
+                 <h1 style={catStyle}>Кот {cat.name}</h1>
+                <img src={logo}/>
+                <p>Порода: {cat.breed}</p>
+                <p>Вес: {cat.weight}кг.</p>
+                <p>Состояние: {cat.isAngry ? "Сердит" :"Дружелюбен"}</p>
+                <button className="btn btn-primary">Погладь кота</button>
             </div>
         )
     }
--- src/components/FuncCat.js
+++ src/components/FuncCat.js
@@ -1,12 +1,21 @@
 function FuncCat (){
 
+    const cat = {name:"Мурзик", breed:"Манул", weight:10, isAngry:false}
+    const logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Manoel.jpg/275px-Manoel.jpg"
+
+    const catStyle = {
+        color: 'blue',
+        backgroundColor:'yellow'
+    }
+
     return(
         <div>
-            <h1>Кот:</h1>
-            <p>Имя: Мурзик</p>
-            <p>Порода: Манул</p>
-            <p>Вес: 10кг.</p>
-            <p>Состояние: Дружелюбен</p>
+                <h1 style={catStyle}>Кот {cat.name}</h1>
+                <img src={logo}/>
+                <p>Порода: {cat.breed}</p>
+                <p>Вес: {cat.weight}кг.</p>
+                <p>Состояние: {cat.isAngry ? "Сердит" :"Дружелюбен"}</p>
+                <button className="btn btn-primary">Погладь кота</button>
         </div>
     )
 }
